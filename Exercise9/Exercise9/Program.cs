52f8155a9c610ebaeb94d36f911175ecc07cf37b
Laila D <laila.daniels@s.capregboces.org>
2024-02-29 10:14:29 -0500

    Working Through Problem 1

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What Row Width");
+            int rowWidth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many Rows");
+           int rowNum = int.Parse(Console.ReadLine());
+
+            for (rowWidth = 0; rowWidth < rowNum; rowWidth++)
+            {
+               
+                Console.WriteLine("*");
+
+
+            }
+
+ int colWidth = int.Parse(Console.ReadLine());
+        }
+    }
+}
