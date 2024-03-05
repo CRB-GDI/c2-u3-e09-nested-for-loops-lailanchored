14fd96658e45ba754619e1ebb7bfd2900e61278d
Laila D <laila.daniels@s.capregboces.org>
2024-03-05 10:03:02 -0500

    Ready For Grading

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -4,31 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("What Row Width : ");
-            int rowWidth = int.Parse(Console.ReadLine());
-
-           
-            //print width of asterisks (left to right)
-            for (int i = 0; rowWidth < i; i++)
-            {
-               
-                Console.Write("*");
-               
-
-
-            }
-           
+            
+          //Prompt for rowNums and rowWidth
             Console.WriteLine(" How many Rows : ");
             int rowNum = int.Parse(Console.ReadLine());
 
-            for (int i = 0; rowNum >= i; i++)
+            Console.Write("What Row Width : ");
+            int rowWidth = int.Parse(Console.ReadLine());
+
+
+            //print number of rows (up and down)
+            //print width of asterisks (left to right)
+            for (int i = 0; rowNum > i; i++)
             {
-                
-                Console.WriteLine("*" ;
+                for (int j = 0; j < rowWidth; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                //end of problem one
 
             }
+            Console.Write("What Width : ");
             
+                int stairWidth = int.Parse(Console.ReadLine());
+            //prompt for width of the stairs
+            for (int k = 1; k <= stairWidth; k++)
+            {
+
+                for (int astAcross = 1; astAcross <= k; astAcross++)
+                {
+                    Console.Write("*");
+                    //[print stairs
+                    
+                }
+
+
+                Console.WriteLine();
             }
+            
+        
+
+
+    }
 
 
     }
